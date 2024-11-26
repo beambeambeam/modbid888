043f0f24411670b97ed174b3fdc6b8fac7d0beb4
beambeambeam <supawitmarayat@gmail.com>
2024-11-26 23:12:51 +0700

    chore: migrate

---
+++ supabase/migrations/20241126161209_dazzling_red_skull.sql
@@ -0,0 +1,108 @@
+CREATE TYPE "public"."role" AS ENUM('member', 'admin');--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "accounts" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"user_id" serial NOT NULL,
+	"password" text NOT NULL,
+	"salt" text NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "betlogs" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"user_id" serial NOT NULL,
+	"timestamp" timestamp NOT NULL,
+	"minigames_id" serial NOT NULL,
+	"bet_amount" real NOT NULL,
+	"bet_result" real NOT NULL,
+	"profit" real NOT NULL,
+	"multiplier" real NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "minigamelogs" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"minigame_id" serial NOT NULL,
+	"timestamp" timestamp NOT NULL,
+	"action" text NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "minigames" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"name" text NOT NULL,
+	"description" text NOT NULL,
+	"win_multiplier" real NOT NULL,
+	"loss_multiplier" real NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "profiles" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"user_id" serial NOT NULL,
+	"display_name" text NOT NULL,
+	"role" "role" DEFAULT 'member' NOT NULL,
+	CONSTRAINT "profiles_user_id_unique" UNIQUE("user_id")
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "sessions" (
+	"id" text PRIMARY KEY NOT NULL,
+	"userId" serial NOT NULL,
+	"expires_at" timestamp with time zone NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "userlogs" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"user_id" serial NOT NULL,
+	"action" text NOT NULL,
+	"timestamp" timestamp NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "users" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"email" text,
+	CONSTRAINT "users_email_unique" UNIQUE("email")
+);
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "accounts" ADD CONSTRAINT "accounts_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE cascade ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "betlogs" ADD CONSTRAINT "betlogs_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE cascade ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "betlogs" ADD CONSTRAINT "betlogs_minigames_id_minigames_id_fk" FOREIGN KEY ("minigames_id") REFERENCES "public"."minigames"("id") ON DELETE cascade ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "minigamelogs" ADD CONSTRAINT "minigamelogs_minigame_id_minigames_id_fk" FOREIGN KEY ("minigame_id") REFERENCES "public"."minigames"("id") ON DELETE cascade ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "profiles" ADD CONSTRAINT "profiles_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE cascade ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "sessions" ADD CONSTRAINT "sessions_userId_users_id_fk" FOREIGN KEY ("userId") REFERENCES "public"."users"("id") ON DELETE cascade ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "userlogs" ADD CONSTRAINT "userlogs_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE cascade ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+CREATE INDEX IF NOT EXISTS "user_id_account_type_idx" ON "accounts" USING btree ("user_id");--> statement-breakpoint
+CREATE INDEX IF NOT EXISTS "user_id_profile_type_idx" ON "profiles" USING btree ("user_id");--> statement-breakpoint
+CREATE INDEX IF NOT EXISTS "role_idx" ON "profiles" USING btree ("role");--> statement-breakpoint
+CREATE INDEX IF NOT EXISTS "sessions_user_id_idx" ON "sessions" USING btree ("userId");--> statement-breakpoint
+CREATE INDEX IF NOT EXISTS "user_id_log_type_idx" ON "userlogs" USING btree ("user_id");
