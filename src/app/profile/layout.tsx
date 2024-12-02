39ea1af012ea275dcb29fd378000545975542972
beambeambeam <supawitmarayat@gmail.com>
2024-12-02 17:51:30 +0700

    fix: redirect errors

--- src/app/profile/layout.tsx
+++ src/app/profile/layout.tsx
@@ -1,11 +1,7 @@
-import { byRoleAccessRedirect } from "~/lib/roles"
-
 export default async function MinigameLayout({
   children,
 }: Readonly<{
   children: React.ReactNode
 }>) {
-  byRoleAccessRedirect(["admin", "member"], "/")
-
   return <>{children}</>
 }
