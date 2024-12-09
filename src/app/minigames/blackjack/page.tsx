cc4c984df9efaae1f7f85b4804f240c980e5287e
beambeambeam <supawitmarayat@gmail.com>
2024-12-10 00:35:22 +0700

    feat: seperator

--- src/app/minigames/blackjack/page.tsx
+++ src/app/minigames/blackjack/page.tsx
@@ -1,6 +1,7 @@
 import { redirect } from "next/navigation"
 
 import Blackjack from "~/app/minigames/blackjack/blackjack"
+import { Separator } from "~/components/ui/separator"
 import { getCurrentBalanceAction } from "~/hooks/bet/actions"
 
 import MinigameTable from "../table"
@@ -13,11 +14,12 @@ async function BlackjackPage() {
   }
 
   return (
-    <div className="w-full grid grid-cols-1 xl:grid-cols-3 h-screen">
-      <div className="flex flex-col col-span-2 items-center justify-start p-4">
+    <div className="w-full grid grid-cols-1 xl:grid-cols-[1fr_auto_1fr] h-screen">
+      <div className="flex flex-col items-center justify-start p-4">
         <Blackjack balance={balance} minigameId={1} />
       </div>
-      <div className="w-full h-full flex items-center px-10 xl:px-0 flex-col justify-center gap-2">
+      <Separator orientation="vertical" className="py-2" />
+      <div className="w-full h-full flex items-center px-10 xl:px-4 flex-col justify-center gap-2">
         <h1 className="w-full font-alagard text-xl">Top profit.</h1>
         <MinigameTable minigameId={1} />
       </div>
