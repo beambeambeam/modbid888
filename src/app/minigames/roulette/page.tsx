9eacf31fbc5f898398ce48113e5dd1da7ee28518
Bank <zxcass145@gmail.com>
2024-12-12 22:33:22 +0700

    fix: roullete
    แก้คำนวณการวางเงินเดิมพนัน ให้หักเงินออกทันที
    แก้บัคตอนมอบรางวัล
    แก้เลขบนวงล้อให้ตรงตามรูปแบบตามจริง
    แก้โค้ดให้สั้นลงในส่วนการคำนวณตอนมอบรางวัล

--- src/app/minigames/roulette/page.tsx
+++ src/app/minigames/roulette/page.tsx
@@ -1,20 +1,13 @@
 import React from "react"
-import { redirect } from "next/navigation"
 
-import Roulette from "~/app/minigames/roulette/roulette"
-import { getCurrentBalanceAction } from "~/hooks/bet/actions"
+import SlotMachine from "../slotmachine/slotmachine"
 
-async function RoulettePage() {
-  const [balance] = await getCurrentBalanceAction()
-
-  if (balance === null || balance === undefined) {
-    return redirect("/")
-  }
+function SlotMachinePage() {
   return (
     <div>
-      <Roulette balance={balance} minigameId={2} />
+      <SlotMachine /> {}
     </div>
   )
 }
 
-export default RoulettePage
+export default SlotMachinePage
