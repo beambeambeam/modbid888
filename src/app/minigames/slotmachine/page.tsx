ad39a8c0703b6d39f1446e7b554c17607a948fe9
Bank <zxcass145@gmail.com>
2024-12-12 22:11:14 +0700

    fix: roullete
    แก้คำนวณการวางเงินเดิมพนัน ให้หักเงินออกทันที
    แก้บัคตอนมอบรางวัล
    แก้เลขบนวงล้อให้ตรงตามรูปแบบตามจริง
    แก้โค้ดให้สั้นลงในส่วนการคำนวณตอนมอบรางวัล

---
+++ src/app/minigames/slotmachine/page.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+import SlotMachine from "../slotmachine/slotmachine"
+
+function SlotMachinePage() {
+  return (
+    <div>
+      <SlotMachine /> {}
+    </div>
+  )
+}
+
+export default SlotMachinePage
