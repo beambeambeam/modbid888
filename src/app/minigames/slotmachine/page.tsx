b61016a84bf2174a59993cf8803b16b8593f9372
Nuggetkub <sittijed.jan@gmail.com>
2024-12-15 21:32:49 +0700

    Revert "fix: edit "slotmachine""
    This reverts commit 7b93ce91d8a52006ab07b1a256d7940cfcb24bde.

--- src/app/minigames/slotmachine/page.tsx
+++ src/app/minigames/slotmachine/page.tsx
@@ -2,8 +2,15 @@ import React from "react"
 
 import SlotMachine from "./SlotMachine"
 
-function App() {
-  return <SlotMachine />
+import "./page.css"
+
+const SlotPage: React.FC = () => {
+  return (
+    <div className="SlotPage">
+      <h1 className="rainbow-title">🍊 Fruity and Friend</h1>
+      <SlotMachine />
+    </div>
+  )
 }
 
-export default App
+export default SlotPage
